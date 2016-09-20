cd0386d24682ec05d8a504892d5b4e1af3da14a5
Sergii <mozgoved88@mail.ru>
2016-09-20 19:22:11 +0300

    Show method at posts done. Helper created

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :moderator
   has_many :comments
   has_many :tags, through: :post_tags
+
+  def self.matching_title_or_content(search)
+    where("title LIKE ? OR content LIKE ?", "%#{search}%", "%#{search}%")
+  end
 end
