7f80b4bb8073a48b5527be34468f655982cbd979
Sergii <mozgoved88@mail.ru>
2016-09-20 20:02:29 +0300

    Creating a new Posts done

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -9,9 +9,19 @@ class Admin::PostsController < Admin::ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.moderator_id = current_moderator.id
+
+    if @post.save
+      redirect_to admin_posts_url, notice: 'Post created'
+    else
+      flash[:alert] = 'There was a problem in creating a post'
+      render :new
+    end
   end
 
   def edit
@@ -25,5 +35,11 @@ class Admin::PostsController < Admin::ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:id, :title, :content, :publish, tag_ids: [])
   end
 end
