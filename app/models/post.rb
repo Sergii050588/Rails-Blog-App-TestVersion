fc0bf8da64efbb49e45935b18430e68af31081be
Sergii <mozgoved88@mail.ru>
2016-09-21 11:56:39 +0300

    validations added to post creation

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,9 @@ class Post < ApplicationRecord
   has_many :post_tags
   has_many :tags, through: :post_tags
 
+  validates :title, presence: :true
+  validates :content, presence: :true
+
   def self.matching_title_or_content(search)
     where("title LIKE ? OR content LIKE ?", "%#{search}%", "%#{search}%")
   end
